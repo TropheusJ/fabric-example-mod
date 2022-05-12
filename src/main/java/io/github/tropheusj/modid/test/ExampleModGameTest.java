6c14da607cddeeb5d3147e7c1941b2e1dc6a1d64
TropheusJ <jverrellijr@icloud.com>
2022-05-11 23:22:26 -0400

    make gametest not fail

--- src/main/java/io/github/tropheusj/modid/test/ExampleModGameTest.java
+++ src/main/java/io/github/tropheusj/modid/test/ExampleModGameTest.java
@@ -1,20 +1,12 @@
 package io.github.tropheusj.modid.test;
 
-import io.github.tropheusj.modid.ExampleMod;
 import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
-import net.minecraft.SharedConstants;
-import net.minecraft.WorldVersion;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
 
 public class ExampleModGameTest implements FabricGameTest {
 	@GameTest(template = EMPTY_STRUCTURE)
-	public void autoMaintainerInfoPrinter(GameTestHelper helper) {
-		helper.fail("a");
-		WorldVersion version = SharedConstants.getCurrentVersion();
-		String name = version.getName();
-		String target = version.getReleaseTarget();
-		String info = String.format("AUTO MAINTAINER INFO || %s || %s", name, target);
-		ExampleMod.LOGGER.info(info);
+	public void exampleTest(GameTestHelper helper) {
+		helper.succeed();
 	}
 }
