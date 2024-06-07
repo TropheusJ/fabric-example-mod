d81ff61b2a71cb10bab8fed2891fcf7b9aba2c2c
TropheusJ <jverrellijr@icloud.com>
2024-06-06 20:20:00 -0400

    1.21

--- src/main/java/io/github/tropheusj/modid/ExampleMod.java
+++ src/main/java/io/github/tropheusj/modid/ExampleMod.java
@@ -16,6 +16,6 @@ public class ExampleMod implements ModInitializer {
 	}
 
 	public static ResourceLocation id(String path) {
-		return new ResourceLocation(ID, path);
+		return ResourceLocation.fromNamespaceAndPath(ID, path);
 	}
 }
--- src/testmod/java/io/github/tropheusj/modid_test/ExampleModTest.java
+++ src/testmod/java/io/github/tropheusj/modid_test/ExampleModTest.java
@@ -16,6 +16,6 @@ public class ExampleModTest implements ModInitializer {
 	}
 
 	public static ResourceLocation id(String path) {
-		return new ResourceLocation(ID, path);
+		return ResourceLocation.fromNamespaceAndPath(ID, path);
 	}
 }
